67e7dd8ba96e55ec84c9c173dcd5a2b07fc3c5fa
Sanchit Samuel <sanchit.samuel@live.com>
2019-04-05 09:50:15 +0530

    *Build funcation changes

--- docker_build.sh
+++ docker_build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-path='/home/chronix/NEETPrep/NEETprepBackend/'
-path_to_run='/home/chronix/NEETPrep/Scripts/run_app.sh'
+path='/home/compiler/NEETPrep/NEETprepBackend/'
+path_to_run='~/NEETPrep/Scripts/run_app.sh'
 if [ "$(id -u)" == "0" ]; then
    echo "This script must not be run as root" 1>&2
    exit 1
 fi
 cd $path
 
-biild () {
+build () {
     echo 'Building on '
     echo `pwd`
     docker build -t registry.gitlab.com/learner.in/learnerwebapi:dev .
