38410b7dede65a469033cacffd59ef1518fb9b76
Sanchit Samuel <sanchit.samuel@live.com>
2019-04-09 10:41:29 +0530

    extented functionality added reaset and help

--- docker_build.sh
+++ docker_build.sh
@@ -11,6 +11,7 @@ build () {
     echo 'Building on '
     echo `pwd`
     docker build -t registry.gitlab.com/learner.in/learnerwebapi:dev .
+    exit 0
 }
 
 compose () {
@@ -18,19 +19,89 @@ compose () {
     docker-compose up
 }
 
+compose_down () {
+    docker-compose down
+}
+
 signup () {
     docker login registry.gitlab.com
+    exit 0
+}
+
+reset () {
+    echo "Are you sure you want to reset docker volumes? (y,N)?"
+    read choice
+    if [[ $choice == 'Y' || $choice == 'y' || $choice == 'yes' ]]; then   
+        echo "Deleting docker volumes, please wait..."
+        compose_down
+        sleep 2
+        docker rmi $(docker images -q) 
+        if [ "$?" != 0 ]; then 
+            echo "Failed at docker rmi"
+            return
+        fi
+        
+        echo "Pruning System"
+        sleep 2
+        sudo docker system prune 
+        if [ "$?" != 0 ]; then 
+            echo "Failed at docker system prune"
+            return
+        fi
+        
+        cd node_modules/
+        docker volume rm neetprepbackend_node_modules
+        if [ "$?" != 0 ]; then 
+            echo "Failed at docker volume rm neetprepbackend_node_modules"
+            return
+        fi
+        
+        docker volume rm neetprepbackend_bower_components
+        if [ "$?" != 0 ]; then 
+            echo "Failed at docker volume rm neetprepbackend_bower_components"
+            return
+        fi
+        cd ..
+        
+        echo "Removing tmpdb"
+        sleep 2
+        sudo rm -rfv tmpdbs
+        
+        echo "Done"
+        echo "Would you like to rebuild? (y,N)"
+        read build_choice
+        if [[ $build_choice == 'Y' || $build_choice == 'y' || $build_choice == 'yes' ]]; then
+            build
+        else
+            exit 0;
+        fi
+    fi
+    
+    exit 0;
 }
 
 if [ "$1" = "run" ]; then
     compose
-fi
+    
+elif [ "$1" = "stop" ]; then
+    compose_down
 
-if [ "$1" = "build" ]; then
+elif [ "$1" = "build" ]; then
     echo 'Starting build'
     build
-fi
 
-if [ "$1" = "signin" ]; then
+elif [ "$1" = "signin" ]; then
     signup
+
+elif [ "$1" = "reset" ]; then
+    reset
+
+else
+    echo "usage: $0 [option]"
+    echo "These are the command line options you can use"
+    echo "  build       To build the project using docker config in the project dir"
+    echo "  signin      To signin to gitlab servers"
+    echo "  run         To run the project in the local environment"
+    echo "  reset       To clean docker volumes"
+    echo "  help        To Show this message"
 fi
